b2d35291259871a4bb818a870dc1409b1b24da74
Ben Wolf <benjamineliaswolf@gmail.com>
2020-09-10 09:03:02 +0200

    Init minitrip project

---
+++ minitrip.py
@@ -0,0 +1,9 @@
+""" Minitrip, a filesystem integrity check tool """
+
+def main():
+	""" The main function for the `minitrip` command """
+	print("Hello world")
+	return 0
+
+if __name__ == "__main__":
+    main()

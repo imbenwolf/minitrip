7100fb56036b184d5090710283e0e77e1d8682ee
Ben Wolf <benjamineliaswolf@gmail.com>
2020-09-10 10:00:21 +0200

    Add walk_paths function to yield all files paths recursively

--- minitrip.py
+++ minitrip.py
@@ -1,9 +1,21 @@
 """ Minitrip, a filesystem integrity check tool """
 
+from pathlib import Path
+
+def walk_paths(path):
+    path = Path(path) if isinstance(path, str) else path
+    for item in path.iterdir():
+        if item.is_dir():
+            yield from walk_paths(item)
+        else:
+            yield item
+
+
 def main():
-	""" The main function for the `minitrip` command """
-	print("Hello world")
-	return 0
+    """ The main function for the `minitrip` command """
+    for item in walk_paths("/home/wolf"):
+        print(item)
+
 
 if __name__ == "__main__":
     main()

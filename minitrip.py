8188a3e80fc2fe122ef55c55559f01d809ee76d2
Ben Wolf <benjamineliaswolf@gmail.com>
2020-09-10 15:12:48 +0200

    Refactor a bit

--- minitrip.py
+++ minitrip.py
@@ -10,9 +10,35 @@ from scan import walk_paths
 
 DEFAULT_DB_PATH = "$HOME/.minitripdb"
 
+
+def add(db, file_path, label, verbose):
+    db.put(hash(file_path), label)
+    if verbose:
+        sys.stderr.write(
+            'New entry: "' + str(file_path) + '" with label "' + label.decode("utf-8") + '"\n')
+
+
+def check(timestamp, hash_value, file_path):
+    if timestamp is None or hash_value is not None and int(hash_value) < int(timestamp):
+        if hash_value is None:
+            sys.stdout.write(
+                'File never seen before: "' + str(file_path) + '"\n')
+        else:
+            sys.stderr.write(
+                'Malware found: "' + str(file_path) + '"\n')
+
+
+def update(db, hash_value, file_path, verbose):
+    if hash_value is None:
+        db.put(hash(file_path), bytes(str(int(time())), "utf-8"))
+    elif verbose and hash_value.decode("utf-8").isdigit():
+        sys.stderr.write('Found file "' + str(file_path) +
+                         '" with value "' + hash_value.decode("utf-8") + '"\n')
+
+
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('PATH', type=click.Path(exists=True), nargs=-1)
-@click.option('-v', '--verbose', is_flag=True)
+@click.option('-v', '--verbose', is_flag=True, type=bool)
 @click.option('-d', '--database', default=DEFAULT_DB_PATH, show_default=True, help="database path")
 @click.option('-a', '--add', 'mode', flag_value='a', is_flag=True, help="Run in add mode (add malware samples)")
 @click.option('-u', '--update', 'mode', flag_value='u', is_flag=True, help="Run in update mode (record timestamps for new files)")
@@ -32,31 +58,24 @@ def main(verbose, database, mode, timestamp, label, path):
     if len(path) == 0:
         path.add(".")
 
-    for path in path: 
-        for item in walk_paths(path):
-            label_db = bytes(item.stem if label is None else label, "utf-8")
+    for path in path:
+        for file_path in walk_paths(path):
+            label_db = bytes(
+                file_path.stem if label is None else label, "utf-8")
+            hash_value = db.get(hash(file_path))
 
             if mode == 'a':
-                db.put(hash(item), label_db)
-                if verbose:
-                    sys.stderr.write('New entry: "' + str(item) + '" with label "' + label_db.decode("utf-8") + '"\n')
-            elif mode == 'c' or mode == 'u':
-                hash_value = db.get(hash(item))
-                
-                if mode == 'c':
-                    if timestamp is None or hash_value is not None and int(hash_value) < int(timestamp):
-                        if hash_value is None:
-                            sys.stdout.write('File never seen before: "' + str(item) + '"\n')
-                        else:
-                            sys.stderr.write('Malware found: "' + str(item) + '"\n')
-                elif mode == 'u':
-                    if hash_value is None:
-                        db.put(hash(item), bytes(str(int(time())), "utf-8"))
-                    elif verbose and hash_value.decode("utf-8").isdigit():
-                        sys.stderr.write('Found file "' + str(item) + '" with value "' + hash_value.decode("utf-8") + '"\n')
+                if hash_value is None:
+                    add(db, file_path, label_db, verbose)
+
+            elif mode == 'c':
+                check(timestamp, hash_value, file_path)
+
+            elif mode == 'u':
+                update(db, hash_value, file_path, verbose)
 
     db.close()
 
 
 if __name__ == "__main__":
-    main() # pylint: disable=no-value-for-parameter
+    main()  # pylint: disable=no-value-for-parameter
